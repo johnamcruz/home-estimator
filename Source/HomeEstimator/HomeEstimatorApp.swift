ee34b27e3f7283ad988347907bcd31b72634c2cf
John Cruz <johnamcruz@gmail.com>
2023-09-18 23:26:53 -1000

    Home estimator stubs

---
+++ Source/HomeEstimator/HomeEstimatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  HomeEstimatorApp.swift
+//  HomeEstimator
+//
+//  Created by John M Cruz on 9/18/23.
+//
+
+import SwiftUI
+
+@main
+struct HomeEstimatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
