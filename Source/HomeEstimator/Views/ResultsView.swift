aab6f2cd166d6474aad3c1963231f71501079dc5
John Cruz <johnamcruz@gmail.com>
2023-09-20 21:35:50 -1000

    updated gauge

--- Source/HomeEstimator/Views/ResultsView.swift
+++ Source/HomeEstimator/Views/ResultsView.swift
@@ -10,23 +10,20 @@ import SwiftUI
 struct ResultsView: View {
     @State var homeValue = 100000.0
     @State private var minValue = 0.0
-    @State private var maxValue = 10000000.0
+    @State private var maxValue = 1000000.0
     
     var body: some View {
-        VStack {
-            Gauge(value: homeValue, in: minValue...maxValue) {
-                Text(LocalizedStringKey(Localization.homeresultvalue))
-            } currentValueLabel: {
-                Text("\(Int(homeValue))")
-            } minimumValueLabel: {
-                Text("\(Int(minValue))")
-            } maximumValueLabel: {
-                Text("\(Int(maxValue))")
-            }
-            .gaugeStyle(.accessoryCircular)
-            .aspectRatio(contentMode: .fit)
+        Gauge(value: homeValue, in: minValue...maxValue) {
+            Text(LocalizedStringKey(Localization.homeresultvalue))
+        } currentValueLabel: {
+            Text("\(Int(homeValue))")
+        } minimumValueLabel: {
+            Text("\(Int(minValue))")
+        } maximumValueLabel: {
+            Text("\(Int(maxValue))")
         }
-        .frame(width: 300, height: 300)
+        .gaugeStyle(SpeedometerGaugeStyle())
+        .aspectRatio(contentMode: .fit)
     }
 }
 
